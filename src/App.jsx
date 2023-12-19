0fff41b6fe80a248a6d4da9d42e3feb897f35a63
Matthew Gsell <matthewdavidgsell@gmail.com>
2023-12-19 17:40:17 -0500

    named a file path more clearly

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Card from "./components/card";
+import Card from "./components/Card.jsx";
 import "./App.css";
 import { PulseLoader } from "react-spinners";
 
